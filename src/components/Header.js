0c9d818baed695bceadaf7f84b0e915f54c67660
Carlos Méndez <carlos.mdez@outlook.com>
2020-02-25 22:35:57 -0600

    Added: Gravatar function.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import gravatar from '../utils/gravatar'
 import '../assets/styles/components/Header.scss'
 import logo from '../assets/static/logo-platzi-video-BW2.png'
 import userIcon from '../assets/static/user-icon.png'
 
-const Header = () => {
+const Header = props => {
+  const {user} = props
+  let renderUser = <img src={userIcon} alt='user' />
+  if(user.email){
+    const gravatarLink = gravatar(user.email)
+    renderUser = <img src={gravatarLink} alt={user.email} />
+  }
   return (
     <header className='header'>
       <Link to='/'>
@@ -12,7 +20,7 @@ const Header = () => {
       </Link>
       <div className='header__menu'>
         <div className='header__menu--profile'>
-          <img src={userIcon} alt='' />
+          {renderUser}
           <p>Perfil</p>
         </div>
         <ul>
@@ -28,4 +36,8 @@ const Header = () => {
   )
 }
 
-export default Header
+const mapStateToProps = (state) => ({
+  user: state.playlistReducer.user
+})
+
+export default connect(mapStateToProps, null)(Header)
